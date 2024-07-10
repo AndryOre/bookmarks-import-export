766039ebf6e7e6a12caa00b8a2c07d1bcce49a40
AndryOre <andryp_@hotmail.com>
2024-07-10 17:25:40 -0400

    ✨ feat: update popup interface for bookmarks converter

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,11 +1,17 @@
-import { CountButton } from "~features/count-button"
+import { ExportBookmarksButton, ImportBookmarksButton } from "~components"
 
 import "~style.css"
 
 function IndexPopup() {
   return (
-    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
-      <CountButton />
+    <div className="plasmo-flex plasmo-flex-col plasmo-px-5 plasmo-py-4 plasmo-w-56 plasmo-gap-3">
+      <h1 className="plasmo-font-bold plasmo-text-base plasmo-text-center">
+        Bookmarks Exporter
+      </h1>
+      <div className="plasmo-flex plasmo-justify-center plasmo-flex-col plasmo-gap-2">
+        <ExportBookmarksButton />
+        <ImportBookmarksButton />
+      </div>
     </div>
   )
 }
