f641dd455e71d3c2a85c767a2ab88ffaf30b78f3
AndryOre <andryp_@hotmail.com>
2024-07-10 17:13:50 -0400

    🔥 refactor: remove unnecessary boilerplate files

--- src/features/count-button.tsx
+++
@@ -1,9 +0,0 @@
-import { useReducer } from "react"
-
-import { Button } from "~components/ui/button"
-
-export const CountButton = () => {
-  const [count, increase] = useReducer((c) => c + 1, 0)
-
-  return <Button onClick={() => increase()}>Count: {count}</Button>
-}
